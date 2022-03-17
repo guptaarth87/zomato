a367454272da7983e9e191cd2cd32d6c32addfe0
Arth Gupta <72153219+guptaarth87@users.noreply.github.com>
2022-03-17 11:18:33 -0700

    Add files via upload

---
+++ Frontend/src/contants.js
@@ -0,0 +1,5 @@
+const DEVELOPMENT_URL = "http://localhost:3010";
+const PRODUCTION_URL = "https://zomato-backe.herokuapp.com";
+
+// exports.API_URL = PRODUCTION_URL;
+exports.API_URL= DEVELOPMENT_URL;
